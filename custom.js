cf1eed08a66ce729e85c05b3721af506dda3f91d
ChanchalSHARMA09 <chanchal.2312051@gmail.com>
2024-10-20 23:50:17 +0530

    I have added score and bullet limit

--- custom.js
+++ custom.js
@@ -1,16 +1,15 @@
 let bullets = 5;
 let count = 0;
-
-function shoot(bird){
     
-
-    if(bullets >=0){
-    bird.style.display = 'none';
-    document.getElementById("currentScore").innerHTML = ++count;
+window.onclick = function(e){
+    if(bullets >0){
+        bullets--;
     document.getElementById("currentBullets").innerHTML = bullets;
-}
-}
+    
+    if(e.target.nodeName == "IMG"){
+        e.target.style.display = 'none';
+        document.getElementById("currentScore").innerHTML = ++count;
+    }
 
-window.onclick = function(){
-    bullets--;
+}
 }
