5f3d17ae2ab86a61263a10abdac0258e84386d35
ChanchalSHARMA09 <chanchal.2312051@gmail.com>
2024-10-21 09:46:43 +0530

    I have added game over page and stored it local storage

--- custom.js
+++ custom.js
@@ -1,7 +1,8 @@
-let bullets = 5; 
+let bullets = 5;
 let score = 0;
+let missedShots = 0;
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const bulletDisplay = document.getElementById("currentBullets");
     const scoreDisplay = document.getElementById("currentScore");
     const fireSound = document.getElementById("fire");
@@ -9,14 +10,57 @@ document.addEventListener("DOMContentLoaded", function() {
     bulletDisplay.textContent = bullets;
     scoreDisplay.textContent = score;
 
-    document.body.addEventListener("click", function(e) {
-        if (bullets > 0 && e.target.tagName === "IMG") {
-            bullets--;
+    document.body.addEventListener("click", function (e) {
+        if (bullets > 0) {
+            bullets--; 
             bulletDisplay.textContent = bullets;
 
-            fireSound.play();
-            e.target.style.display = 'none';
-            scoreDisplay.textContent = ++score;
+            if (e.target.tagName === "IMG") {
+                fireSound.play();
+                e.target.style.display = 'none';
+                scoreDisplay.textContent = ++score;
+
+                
+                localStorage.setItem('shotBirds', score);
+            } else {
+                
+                missedShots++;
+                localStorage.setItem('missedShots', missedShots);
+            }
+            if (bullets === 0) {
+                gameOver();
+            }
         }
     });
 });
+
+
+function gameOver() {
+    alert('Game Over! You have no more bullets.');
+    window.location.href = 'gameover.html';
+}
+
+
+window.addEventListener("load", function () {
+    if (!localStorage.getItem('shotBirds')) {
+        localStorage.setItem('shotBirds', 0);
+    }
+    if (!localStorage.getItem('missedShots')) {
+        localStorage.setItem('missedShots', 0);
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
